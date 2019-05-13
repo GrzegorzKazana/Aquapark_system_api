1424b417273a3f7050b736a0af685ca6812717d6
Marek Hermansa <marekhermansa@gmail.com>
2019-05-13 23:21:11 +0200

    Added methods for adding PeriodicDiscounts, SocialClassDiscounts and ZonesWithTickets

--- AquaparkSystemApi/Controllers/DiscountsController.cs
+++ AquaparkSystemApi/Controllers/DiscountsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using AquaparkSystemApi.Models;
 using AquaparkSystemApi.Models.Dtos;
 
 namespace AquaparkSystemApi.Controllers
@@ -15,9 +16,53 @@ namespace AquaparkSystemApi.Controllers
             _dbContext = new AquaparkDbContext();
         }
 
+        [AcceptVerbs("POST")]
+        [ActionName("AddSocialClassDiscounts")]
+        public IEnumerable<SocialClassDiscountDto> AddSocialClassDiscounts(IEnumerable<SocialClassDiscount> socialClassDiscounts)
+        {
+            try
+            {
+                _dbContext.Positions.ToList().ForEach(x => x.SocialClassDiscount = null);
+
+                _dbContext.SaveChanges();
+
+                _dbContext.SocialClassDiscounts.RemoveRange(_dbContext.SocialClassDiscounts);
+                _dbContext.SocialClassDiscounts.AddRange(socialClassDiscounts);
+                _dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return this.GetAllSocialClassDiscounts();
+            }
+
+            return this.GetAllSocialClassDiscounts();
+        }
+
+        [AcceptVerbs("POST")]
+        [ActionName("AddPeriodicDiscounts")]
+        public IEnumerable<PeriodicDiscountDto> AddPeriodicDiscounts(IEnumerable<PeriodicDiscount> periodicDiscounts)
+        {
+            try
+            {
+                _dbContext.Tickets.ToList().ForEach(x => x.PeriodicDiscount = null);
+                _dbContext.Positions.ToList().ForEach(x => x.PeriodicDiscount = null);
+                _dbContext.SaveChanges();
+
+                _dbContext.PeriodicDiscounts.RemoveRange(_dbContext.PeriodicDiscounts);
+                _dbContext.PeriodicDiscounts.AddRange(periodicDiscounts);
+                _dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return this.GetAllPeriodicDiscount();
+            }
+
+            return this.GetAllPeriodicDiscount();
+        }
+
         [AcceptVerbs("GET")]
-        [ActionName("GetAllPeriodicDoscunts")]
-        public IEnumerable<PeriodicDiscountDto> GetAllPeriodicDoscunts()
+        [ActionName("GetAllPeriodicDiscount")]
+        public IEnumerable<PeriodicDiscountDto> GetAllPeriodicDiscount()
         {
             List<PeriodicDiscountDto> periodicDiscountDtos = new List<PeriodicDiscountDto>();
             try
--- AquaparkSystemApi/Controllers/ZonesController.cs
+++ AquaparkSystemApi/Controllers/ZonesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using AquaparkSystemApi.Models;
 using AquaparkSystemApi.Models.Dtos;
 
 namespace AquaparkSystemApi.Controllers
@@ -14,6 +15,82 @@ namespace AquaparkSystemApi.Controllers
         {
             _dbContext = new AquaparkDbContext();
         }
+
+        [AcceptVerbs("POST")]
+        [ActionName("AddZonesWithTickets")]
+        public IEnumerable<ZoneWithTicketsDto> AddZonesWithTickets(IEnumerable<ZoneWithTicketsDto> zonesWithTickets)
+        {
+            try
+            {
+                // remove Zone references
+
+                _dbContext.Attractions.ToList().ForEach(x => x.Zone = null);
+                _dbContext.ZoneHistories.ToList().ForEach(x => x.Zone = null);
+                _dbContext.Tickets.ToList().ForEach(x => x.Zone = null);
+                _dbContext.SaveChanges();
+
+                // remove Ticket references
+
+                _dbContext.Positions.ToList().ForEach(x => x.Ticket = null);
+                _dbContext.SaveChanges();
+
+                // remove all records from Zone and Ticket table
+
+                _dbContext.Zones.RemoveRange(_dbContext.Zones);
+                _dbContext.Tickets.RemoveRange(_dbContext.Tickets);
+                _dbContext.SaveChanges();
+
+                // Add new records to Zone table
+
+                var zones = zonesWithTickets.Select(x => new Zone()
+                {
+                    Name = x.ZoneName
+                });
+                _dbContext.Zones.AddRange(zones);
+                _dbContext.SaveChanges();
+
+                // Add new records to Ticket table
+
+                var tickets = new List<Ticket>();
+                zonesWithTickets
+                    .ToList()
+                    .ForEach(z => z.TicketTypes
+                        .ToList()
+                        .ForEach(t => tickets.Add(new Ticket()
+                        {
+                            Name = t.TicketTypeName,
+                            Price = t.Price,
+                            Zone = _dbContext.Zones
+                                    .Where(j => j.Name == z.ZoneName)
+                                    .FirstOrDefault()
+                                    ?? new Zone()
+                                    {
+                                        Name = z.ZoneName
+                                    },
+                            PeriodicDiscount = t.PeriodDiscount == null
+                                ? null : new PeriodicDiscount()
+                                {
+                                    StartTime = t.PeriodDiscount.StartTimeDate,
+                                    FinishTime = t.PeriodDiscount.FinishTimeDate,
+                                    Value = t.PeriodDiscount.Value
+                                },
+                            StartHour = t.StartHour,
+                            EndHour = t.EndHour,
+                            Days = t.Days,
+                            Months = t.Months
+                        })
+                        ));
+                _dbContext.Tickets.AddRange(tickets);
+                _dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return this.GetAllZonesWithTickets();
+            }
+
+            return this.GetAllZonesWithTickets();
+        }
+
 
         [AcceptVerbs("GET")]
         [ActionName("GetAllZones")]
@@ -48,12 +125,12 @@ namespace AquaparkSystemApi.Controllers
                     ZoneId = i.Id,
                     Name = i.Name,
                     Attractions = _dbContext.Attractions.
-                        Where(j=> j.Zone.Id == i.Id).
-                        Select(j=> new AttractionPrimaryInformationDto()
-                    {
-                        AttractionId = j.Id,
-                        Name = j.Name
-                    })
+                        Where(j => j.Zone.Id == i.Id).
+                        Select(j => new AttractionPrimaryInformationDto()
+                        {
+                            AttractionId = j.Id,
+                            Name = j.Name
+                        })
                 }).ToList();
             }
             catch (Exception)
