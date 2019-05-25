9b8307ce6658e7cd4354912428c88b6147263ed0
Rafał Dębski <rafadeb171@student.polsl.pl>
2019-05-25 21:42:52 +0200

    Deleting unnecessary code

--- AquaparkSystemApi/Global.asax.cs
+++ AquaparkSystemApi/Global.asax.cs
@@ -18,8 +18,6 @@ namespace AquaparkSystemApi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
-            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
         }
     }
 }
